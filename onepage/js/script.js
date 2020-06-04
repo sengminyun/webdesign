a42fb09058ac951b4f3e40b43992011fd86a3445
sengminyun <64954108+sengminyun@users.noreply.github.com>
2020-06-04 17:58:37 +0900

    art

--- onepage/js/script.js
+++ onepage/js/script.js
@@ -33,6 +33,10 @@ $(function(){
         $('.tab-txt-3').css('display','block')
         $('.tab-img-3').css('opacity','1');
     });
+    $('.button').click(function(ev){
+        ev.preventDefault();
+        $(this).toggleClass('active');
+    });
 });
 
 
@@ -75,3 +79,24 @@ $(function(){
       return false;
    });
 });
+var mapContainer = document.getElementById('.map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);    
